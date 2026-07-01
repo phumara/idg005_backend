2d68df677a7ac3da9660d716c9d625965dddeec7
phumara <mot.phum@gmail.com>
2026-07-01 09:54:33 +0700

    create laravel project in docker

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [MessageController::class, 'index'])->name('messages.index');
-Route::post('/messages', [MessageController::class, 'store'])->name('messages.store');
+Route::get('/', function () {
+    return view('welcome');
+});
