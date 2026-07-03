9ff28d57f68a9e058a7874ab78c439d404c68c70
phumara <mot.phum@gmail.com>
2026-07-03 09:20:34 +0700

    authcrontroller error'

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/signin', [AuthController::class, 'signin']);
+Route::post('/signout', [AuthController::class, 'signout']);
+Route::get('/verify', [AuthController::class, 'verify']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
